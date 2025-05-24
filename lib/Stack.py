d974ab55b53306406e5b07071e3a29beb42d3e5f
Chadwick Koo <chadwickkoo@outlook.com>
2025-05-24 11:14:07 +0300

    Modified files

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,45 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.stack = []
+        self.limit = limit
+        self.items = []  # keep this to match tests!
+
+        if items:
+            for char in items:
+                self.stack.append(char)
+                self.items.append(char)  # sync items
 
     def isEmpty(self):
-        pass
+        return len(self.stack) == 0
 
     def push(self, item):
-        pass
+        if len(self.stack) < self.limit:
+            self.stack.append(item)
+            self.items.append(item)  # sync items
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            popped = self.stack.pop()
+            self.items.pop()  # sync items
+            return popped
 
     def peek(self):
-        pass
-    
+        if not self.isEmpty():
+            return self.stack[-1]
+        else:
+            return None
+
     def size(self):
-        pass
+        return len(self.stack)
 
     def full(self):
-        pass
+        return len(self.stack) == self.limit
 
     def search(self, target):
-        pass
+        if target in self.stack:
+            index_from_bottom = self.stack.index(target)
+            index_from_top = len(self.stack) - index_from_bottom - 1
+            return index_from_top
+        else:
+            return -1
